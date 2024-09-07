975482777c3fa3b6c94174d8779ca9cbe2370d25
bren112 <brenomarinho1610@gmail.com>
2024-09-06 21:58:17 -0300

    Novo

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ document.getElementById('prevBtn').addEventListener('click', () => {
 function updateCarousel() {
     carousel.style.setProperty('--position', currentPosition);
 }
+let currentPosition2 = 1; // Posição inicial do carrossel
+const totalItems2 = 8; // Total de itens no carrossel
+const carousel2 = document.getElementById('carousel2');
+
+document.getElementById('nextBtn2').addEventListener('click', () => {
+    currentPosition2 = currentPosition2 < totalItems2 ? currentPosition2 + 1 : 1; // Volta para o primeiro se for o último
+    updateCarousel2();
+});
+
+document.getElementById('prevBtn2').addEventListener('click', () => {
+    currentPosition2 = currentPosition2 > 1 ? currentPosition2 - 1 : totalItems; // Vai para o último se for o primeiro
+    updateCarousel2();
+});
+
+function updateCarousel2() {
+    carousel2.style.setProperty('--position', currentPosition2);
+}
